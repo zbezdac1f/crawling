96ff2ff22062241329b78d9168f32ec91f0a7de2
zvezdac1f <dhehdgus1231@naver.com>
2024-10-23 10:38:34 +0900

    마트잡 크롤링 예외처리
    요소를 가져올 수 없어도 무시하고 동작하게 설정, 리턴값 '' 으로 수정

--- martjob_searchMart.js
+++ martjob_searchMart.js
@@ -5,7 +5,7 @@ const router = require('express').Router();
 
 // 기본 URL과 페이지 수
 const baseUrl = 'https://www.martjob.co.kr/job/guin.asp';
-const totalPages = 3;  // 페이지 끝 범위 
+const totalPages = 10;  // 페이지 끝 범위 
 
 // HTML을 가져오는 함수
 const fetchHtml = async (url) => {
@@ -28,12 +28,12 @@ const extractWorkTime = async (jobUrl) => {
         const worktimeText = $('#containerWrap > div:nth-child(6) > div').text().trim();
 
         // '근무시간 |' 이 포함된 부분만 추출
-        const worktime = worktimeText.split('근무시간 |')[1]?.split('\n')[0]?.trim() || 'Not available';
+        const worktime = worktimeText.split('근무시간 |')[1]?.split('\n')[0]?.trim() || '';
 
         return worktime;
     } catch (error) {
         console.error(`Error extracting work time from ${jobUrl}:`, error);
-        return 'Error fetching work time'; // 에러 발생시 에러 메시지 반환
+        return ''; // 에러 발생시 빈 문자열 반환
     }
 };
 
@@ -46,10 +46,10 @@ const extractPhoneNumber = async (jobUrl) => {
         // 전화번호 정보를 해당 CSS 선택자로 추출
         const phoneNumberText = $('#containerWrap > div:nth-child(10) > span.disflex.flexv-column.g-table.w100 > span:nth-child(2) > span:nth-child(2)').text().trim();
 
-        return phoneNumberText || 'Not available';
+        return phoneNumberText || '';
     } catch (error) {
         console.error(`Error extracting phone number from ${jobUrl}:`, error);
-        return 'Error fetching phone number'; // 에러 발생시 에러 메시지 반환
+        return ''; // 에러 발생시 빈 문자열 반환
     }
 };
 
@@ -61,12 +61,12 @@ const extractSalary = async (jobUrl) => {
 
         // '임금' 정보를 해당 CSS 선택자로 추출
         const salaryText = $('#containerWrap > div:nth-child(6) > div > font:nth-child(10)').text().trim();
-        const salary = salaryText.split('임금조건 |')[1]?.trim() || 'Not available';
+        const salary = salaryText.split('임금조건 |')[1]?.trim() || '';
 
-        return salary || 'Not available';
+        return salary;
     } catch (error) {
         console.error(`Error extracting salary from ${jobUrl}:`, error);
-        return 'Error fetching salary'; // 에러 발생시 에러 메시지 반환
+        return ''; // 에러 발생시 빈 문자열 반환
     }
 };
 
@@ -79,10 +79,10 @@ const extractWorkLocation = async (jobUrl) => {
         // 근무 위치 정보를 해당 CSS 선택자로 추출
         const workLocationText = $('#containerWrap > div:nth-child(10) > span.disflex.flexv-column.g-table.w100 > span.disflex.flexv-top.mt10 > span:nth-child(2)').text().trim();
 
-        return workLocationText || 'Not available';
+        return workLocationText || '';
     } catch (error) {
         console.error(`Error extracting work location from ${jobUrl}:`, error);
-        return 'Error fetching work location'; // 에러 발생시 에러 메시지 반환
+        return ''; // 에러 발생시 빈 문자열 반환
     }
 };
 
@@ -99,59 +99,42 @@ const extractJobDetails = async (html) => {
         const rowId = $(element).attr('id');
         const martName = $(`#${rowId} > td:nth-child(3) > span.gconame > span`).text().trim();
 
-        // 회사명 위치 수정
         const location = $(element).find('td:nth-child(2)').text().trim();
         const titleElement = $(element).find('td:nth-child(3) a');
         const title = titleElement.text().trim();
         const link = titleElement.attr('href');
         const fullLink = `https://www.martjob.co.kr/${link}`;
 
-        //console.log(fullLink)
+        if (title) {
+            const worktime = await extractWorkTime(fullLink);
+            const phoneNumber = await extractPhoneNumber(fullLink);
+            const salary = await extractSalary(fullLink);
+            const workLocation = await extractWorkLocation(fullLink);
 
-        // 필터링된 키워드 목록
-        const excludeKeywords = ['관리', '유통', '관리 진열'];
-        const includeKeywords = ['배송', '배달'];
-
-        // title이 포함 또는 제외 키워드에 따라 필터링
-        const shouldExclude = excludeKeywords.some(keyword => title.includes(keyword));
-        const shouldInclude = includeKeywords.some(keyword => title.includes(keyword));
-
-        if (title && !shouldExclude && shouldInclude) {
-            const worktime = await extractWorkTime(fullLink); // 각 공고의 worktime 추출
-            const phoneNumber = await extractPhoneNumber(fullLink); // 각 공고의 전화번호 추출
-            const salary = await extractSalary(fullLink); // 각 공고의 임금 추출
-            const workLocation = await extractWorkLocation(fullLink); // 각 공고의 근무 위치 추출
-
-            if (worktime !== 'Not available') {
-                jobDetails.push({
-                    martName: martName,       // gconame에서 추출한 텍스트
-                    deadline: deadline,       // 수정일
-                    location: location,       // 회사명
-                    title: title,             // 채용제목
-                    martSize: martSize,       // 생성일
-                    worktime: worktime,       // 근무 시간
-                    phoneNumber: phoneNumber,
-                    salary: salary, // 전화번호
-                    work_location: workLocation, // 근무 위치
-                    link: fullLink            // 채용공고 링크
-                });
-            }
+            jobDetails.push({
+                martName: martName || '',       // gconame에서 추출한 텍스트
+                deadline: deadline || '',       // 수정일
+                location: location || '',       // 회사명
+                title: title || '',             // 채용제목
+                martSize: martSize || '',       // 생성일
+                worktime: worktime,             // 근무 시간
+                phoneNumber: phoneNumber,       // 전화번호
+                salary: salary,                 // 임금
+                work_location: workLocation,     // 근무 위치
+                link: fullLink                  // 채용공고 링크
+            });
         }
     }).get();  // .map()의 결과를 배열로 변환
 
     await Promise.all(promises);
-    console.log(jobDetails)
     return jobDetails;
-
 };
 
 // 메인 함수
 const main = async () => {
     try {
         const allJobDetails = [];
-        console.log(allJobDetails)
         // 페이지를 반복하여 데이터 수집
-        //페이지 시작 범위
         for (let page = 1; page <= totalPages; page++) {
             console.log(` ${page} 페이지`);
             const html = await fetchHtml(`${baseUrl}?fset=job-118&listorder=&aream=&areagum=&jobcode=08&midkeyw=&gubunchk=0&grade=&areacode0=&areagu_code0=&listRow=30&page=${page}`);
@@ -160,7 +143,7 @@ const main = async () => {
         }
 
         // 결과를 파일에 저장
-        fs.writeFileSync('martJob3.json', JSON.stringify(allJobDetails, null, 2), 'utf8');
+        fs.writeFileSync('martJob1.json', JSON.stringify(allJobDetails, null, 2), 'utf8');
         console.log('끝');
     } catch (error) {
         console.error('Error:', error);
